4e36c290601ca917654e35066c921c8bce28bf66
shazm12 <64892076+shazm12@users.noreply.github.com>
2021-02-12 23:59:08 +0530

    Adding source files

---
+++ server.js
@@ -0,0 +1,71 @@
+const express =require('express');
+const path = require('path');
+const http = require('http');
+const app = express();
+const socketio =require('socket.io');
+
+const formatMessage = require('./utils/messages');
+const { userJoin ,getCurrentUser ,userLeave ,getRoomUsers } = require('./utils/users');
+const { format } = require('path');
+
+const port = 3000 || process.env.PORT;
+
+const server = http.createServer(app);
+
+const io =socketio(server);
+
+const botName = 'Chat Code Bot';
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Run when a client connects
+io.on('connection', socket => {
+
+    socket.on('joinRoom', ({username, room}) => {
+
+        const user = userJoin(socket.id, username,room);
+        socket.join(user.room);
+        
+        //Welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to Chat Code!'));
+
+        //Broadcast when a user connects
+        socket.broadcast.to(user.room).emit('message',formatMessage(botName, `${user.username} has joined a chat`));
+
+        //Send user and rooom info
+        io.to(user.room).emit('roomUsers' , {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+
+    });
+
+
+    //Listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+
+    })
+
+    //Runs when client disconnects;
+    socket.on('disconnect', () => {
+
+        const user = userLeave(socket.id);
+        if(user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+            //Send user and rooom info
+            io.to(user.room).emit('roomUsers' , {
+                room: user.room,
+               users: getRoomUsers(user.room)
+            });
+        } 
+    })
+
+});
+
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
